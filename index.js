bdc527bb4362bd95a7d845f7b09649d08f634ed2
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2023-10-27 03:08:44 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
+// To pass the tests, don't forget to return your fetch!
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  const apiUrl = 'https://anapioficeandfire.com/api/books'
+  const options = {
+    method: 'GET',
+    headers: {
+      "Text-Content": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify()
+  }
+  fetch(apiUrl, options)
+  .then(res => res.json())
+  .then(data => renderBooks(data))
+  .catch(error => console.error('Error:', error))
+
+  function renderBooks(books){
+    const bookList = document.createElement('ul')
+    const main = document.querySelector('main')
+    
+    books.forEach(book => {
+      const list = document.createElement('li')
+      list.textContent = book.name;
+      bookList.appendChild(list)
+      main.appendChild(bookList)
+      document.body.appendChild(main)
+    })
+  }
 }
 
 function renderBooks(books) {
