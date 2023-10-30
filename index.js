e9ea043f69c6447c89cec567dddc371d1753c707
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2023-10-30 12:02:06 +0300

    Assignment redone but I have not figured out why the html is running but the 2nd test is not passing

--- index.js
+++ index.js
@@ -1,40 +1,21 @@
 // To pass the tests, don't forget to return your fetch!
 function fetchBooks() {
   const apiUrl = 'https://anapioficeandfire.com/api/books'
-  const options = {
-    method: 'GET',
-    headers: {
-      "Text-Content": "application/json",
-      Accept: "application/json"
-    },
-    body: JSON.stringify()
-  }
-  fetch(apiUrl, options)
-  .then(res => res.json())
-  .then(data => renderBooks(data))
-  .catch(error => console.error('Error:', error))
 
-  function renderBooks(books){
-    const bookList = document.createElement('ul')
-    const main = document.querySelector('main')
-    
-    books.forEach(book => {
-      const list = document.createElement('li')
-      list.textContent = book.name;
-      bookList.appendChild(list)
-      main.appendChild(bookList)
-      document.body.appendChild(main)
-    })
-  }
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => renderBooks(data))
+    .catch(error => console.error('error fetching books:', error))
 }
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+function renderBooks(books){
+  const main = document.querySelector('main')
+
+  books.map(book => {
+    const h2 = document.createElement('h2')
+    h2.textContent = book.name;
+    main.appendChild(h2)
+  })
 }
 
 document.addEventListener('DOMContentLoaded', function() {
