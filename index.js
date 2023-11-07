43e18c368f2f4bf2a338f3b10318eb6a924e03b6
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2023-11-07 18:04:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
-// To pass the tests, don't forget to return your fetch!
 function fetchBooks() {
-  const apiUrl = 'https://anapioficeandfire.com/api/books'
-
-  fetch(apiUrl)
+  return fetch('https://anapioficeandfire.com/api/books')
     .then(response => response.json())
-    .then(data => renderBooks(data))
-    .catch(error => console.error('error fetching books:', error))
+    .then(books => renderBooks(books))
+    .catch(error => console.error('error fetching books:', error));
 }
 
-function renderBooks(books){
-  const main = document.querySelector('main')
-
-  books.map(book => {
-    const h2 = document.createElement('h2')
+function renderBooks(books) {
+  const main = document.querySelector('main');
+  books.forEach(book => {
+    const h2 = document.createElement('h2');
     h2.textContent = book.name;
-    main.appendChild(h2)
-  })
+    main.appendChild(h2);
+  });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+fetchBooks()
